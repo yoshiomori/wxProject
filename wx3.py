ca3bab96c431325208d6ca7384cb71b8f2d29b22
Yoshio Mori <yoshio.mori@hotmail.com>
2016-12-23 11:08:36 -0200

    Extendendo Frame

--- wx3.py
+++ wx3.py
@@ -5,11 +5,11 @@ import wx
 class MyFrame(wx.Frame):
     """ We simply derive a new class of Frame. """
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        wx.Frame.__init__(self, parent, title=title, size=(200, 100))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.Show(True)
 
-app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
+app = wx.App()  # Create a new app, don't redirect stdout/stderr to a window.
+frame = MyFrame(None, "Hello World")  # A Frame is a top-level window.
 frame.Show(True)     # Show the frame.
 app.MainLoop()
