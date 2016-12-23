d4bde27f92b7402dc414e1085b4b5a114c96ec62
Yoshio Mori <yoshio.mori@hotmail.com>
2016-12-23 17:02:23 -0200

    Usando EVT_BUTTON.

--- wx3.py
+++ wx3.py
@@ -8,12 +8,13 @@ def embaixo(elemento):
 app = wx.App()  # Create a new app, don't redirect stdout/stderr to a window.
 frame = wx.Frame(None, title="Hello World", size=(800, 600))  # A Frame is a top-level window.
 panel = wx.Panel(frame)
-text_ctrl1 = wx.TextCtrl(panel, style=wx.TE_MULTILINE, pos=(0, 0), size=(100, 100))
-text_ctrl2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE, pos=(100, 0), size=(100, 100))
-button1 = wx.Button(panel, pos=(0, 100), size=(200, 100))
+text_ctrl1 = wx.TextCtrl(panel, style=wx.TE_MULTILINE, pos=(0, 0))
+text_ctrl2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE, pos=embaixo(text_ctrl1))
+button1 = wx.Button(panel, pos=embaixo(text_ctrl2))
+panel.Bind(wx.EVT_BUTTON, lambda event: text_ctrl1.AppendText(" oi"), button1)
 panel.SetSize(panel.GetBestSize())
-button2 = wx.Button(frame, pos=embaixo(panel), size=(200, 100))
-button3 = wx.Button(frame, pos=embaixo(button2), size=(200, 100))
+button2 = wx.Button(frame, pos=embaixo(panel))
+button3 = wx.Button(frame, pos=embaixo(button2))
 frame.SetSize(frame.GetBestSize())
 frame.Show(True)
 app.MainLoop()
