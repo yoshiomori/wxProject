e30ac97e08b61fcb0eb397e1647a9cbcfbf4e31f
Yoshio Mori <yoshio.mori@hotmail.com>
2016-12-23 11:05:51 -0200

    Comandos que podem ser executados.

---
+++ wx3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import wx
+
+
+class MyFrame(wx.Frame):
+    """ We simply derive a new class of Frame. """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Show(True)
+
+app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
+frame.Show(True)     # Show the frame.
+app.MainLoop()
