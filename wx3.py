01ff016ff01c0480ccb15622d1b8e32c63d97264
Yoshio Mori <yoshio.mori@hotmail.com>
2016-12-24 15:18:25 -0200

    Adicionando Calendar.

--- wx3.py
+++ wx3.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import wx
+from wx.lib.calendar import Calendar
 
 # funcao que recebe um elemento  com posicao e tamanho e retorna a posicao embaixo dele.
 def embaixo(elemento):
     return elemento.GetPosition()[0], elemento.GetPosition()[1] + elemento.GetSize()[1]
+
+# funcao que recebe um elemento com posicao e tamanho e retorna a posicao na direita.
+def na_direita(elemento):
+    return elemento.GetPosition()[0] + elemento.GetBestSize()[0], elemento.GetPosition()[1]
 
 # Create a new app, don't redirect stdout/stderr to a window.
 app = wx.App()
@@ -23,9 +28,6 @@ text_ctrl2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE, pos=embaixo(text_ctrl1))
 # Criando um Botao embaixo do text_ctrl2.
 button1 = wx.Button(panel, pos=embaixo(text_ctrl2))
 
-# Criando um manipulador ao EVT_BUTTON.
-panel.Bind(wx.EVT_BUTTON, lambda event: text_ctrl1.AppendText(" oi"), button1)
-
 # Calculando o melhor tamanho do Panel e configurando o tamanho do panel com isso.
 panel.SetSize(panel.GetBestSize())
 
@@ -34,6 +36,15 @@ button2 = wx.Button(frame, pos=embaixo(panel))
 
 # Adicionando outro botao embaixo do button2.
 button3 = wx.Button(frame, pos=embaixo(button2))
+
+# Calendar
+calendar = Calendar(frame, pos=na_direita(panel))
+
+# Criando um manipulador para o botao 2
+frame.Bind(wx.EVT_BUTTON, lambda event: text_ctrl2.AppendText("%d " % calendar.GetDay()), button2)
+
+# Criando um manipulador ao EVT_BUTTON.
+panel.Bind(wx.EVT_BUTTON, lambda event: text_ctrl1.AppendText(" oi"), button1)
 
 # Calculando melhor tamanho.
 size = frame.GetBestSize()
